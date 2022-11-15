40745e42887f71a0db337302630f9542ab307d41
Atul raj <atulraj.ar123@gmail.com>
2022-11-15 16:28:43 +0530

    files of first class

---
+++ square.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+class square
+{
+	public static void main(String []ag)
+	{
+	int n,s;
+	n=Integer.parseInt(JOptionPane.showInputDialog("Enter a number"));
+	s=n*n;
+	System.out.println("square="+s);
+	}
+}
